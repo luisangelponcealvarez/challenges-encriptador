dc15a0b1100dfdc146a721048a383078811ecca3
Luis angel ponce <poncealvarezluisangel4@gmail.com>
2023-05-09 21:26:29 -0600

    acomodo todos los stily en uno solo

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,91 +1,55 @@
 function focus() {
-    let input = document.getElementById("input-texto");
-    input.focus();
+  let input = document.getElementById("input-texto");
+  input.focus();
 }
 
 function value() {
-    let input = document.getElementById("input-texto");
-    input.value = "";
+  let input = document.getElementById("input-texto");
+  input.value = "";
 }
 
 function encriptar() {
-    let texto = document.getElementById("input-texto").value;
-    let txt_cifrado = texto.replace(/e/gm, "enter");
-    txt_cifrado = txt_cifrado.replace(/o/gm, "ober");
-    txt_cifrado = txt_cifrado.replace(/i/gm, "imes");
-    txt_cifrado = txt_cifrado.replace(/a/gm, "ai");
-    txt_cifrado = txt_cifrado.replace(/u/gm, "ufat");
+  let texto = document.getElementById("input-texto").value;
+  let txt_cifrado = texto.replace(/e/gm, "enter");
+  txt_cifrado = txt_cifrado.replace(/o/gm, "ober");
+  txt_cifrado = txt_cifrado.replace(/i/gm, "imes");
+  txt_cifrado = txt_cifrado.replace(/a/gm, "ai");
+  txt_cifrado = txt_cifrado.replace(/u/gm, "ufat");
 
-    if (/[^a-zñ ]/.test(texto)) {
-        Swal.fire({
-            icon: 'error',
-            iconColor: '#b9ab9c',
-            background: '#E3E0DE',
-            title: 'Oops...',
-            confirmButtonColor: '#b9ab9c',
-            text: 'Solo se permiten letras minusculas y sin acento',
-          });
-    }
-    else if (texto.length === 0) {
-        Swal.fire({
-            icon: 'error',
-            iconColor: '#b9ab9c',
-            background: '#E3E0DE',
-            title: 'Oops...',
-            confirmButtonColor: '#b9ab9c',
-            text: 'El campo de texto está vacio, escriba el texto que desea encriptar',
-          });
-    }
-    else {
-        document.getElementById("text-mensaje").style.display = "inline-block";
-        document.getElementById("texto-1").style.display = "none";
-        document.getElementById("texto-1").style.display = "none";
-        document.getElementById("output-texto").innerHTML = txt_cifrado;
-        
-        value();
-    }
+  if (/[^a-zñ ]/.test(texto)) {
+  } else if (texto.length === 0) {
+  } else {
+    document.getElementById("text-mensaje").style.display = "inline-block";
+    document.getElementById("texto-1").style.display = "none";
+    document.getElementById("texto-1").style.display = "none";
+    document.getElementById("output-texto").innerHTML = txt_cifrado;
+
+    value();
+  }
 }
 
 function desencriptar() {
-    let texto = document.getElementById("input-texto").value;
-    let txt_cifrado = texto.replace(/enter/gm, "e");
-    txt_cifrado = txt_cifrado.replace(/ober/gm, "o");
-    txt_cifrado = txt_cifrado.replace(/imes/gm, "i");
-    txt_cifrado = txt_cifrado.replace(/ai/gm, "a");
-    txt_cifrado = txt_cifrado.replace(/ufat/gm, "u");
+  let texto = document.getElementById("input-texto").value;
+  let txt_cifrado = texto.replace(/enter/gm, "e");
+  txt_cifrado = txt_cifrado.replace(/ober/gm, "o");
+  txt_cifrado = txt_cifrado.replace(/imes/gm, "i");
+  txt_cifrado = txt_cifrado.replace(/ai/gm, "a");
+  txt_cifrado = txt_cifrado.replace(/ufat/gm, "u");
 
-    if (texto.length === 0) {
-        Swal.fire({
-            icon: 'error',
-            iconColor: '#b9ab9c',
-            background: '#E3E0DE',
-            title: 'Oops...',
-            confirmButtonColor: '#b9ab9c',
-            text: 'El campo de texto está vacio, escriba el texto que desea desencriptar',
-        });
-    }
-
-    else {
+  if (texto.length === 0) {
+  } else {
     document.getElementById("texto-1").style.display = "none";
     document.getElementById("texto-1").style.display = "none";
     document.getElementById("texto-1").style.display = "none";
-    document.getElementById("output-texto").innerHTML = txt_cifrado;    
+    document.getElementById("output-texto").innerHTML = txt_cifrado;
     value();
-    }
+  }
 }
 
 function copiar() {
-    let contenido = document.querySelector("#output-texto");
-    contenido.select();
-    document.execCommand("copy");
-    Swal.fire({
-        icon: 'success',
-        iconColor: '#b9ab9c',
-        background: '#E3E0DE',
-        title: '¡Bien!',
-        confirmButtonColor: '#b9ab9c',
-        text: 'Texto copiado correctamente',
-    });
+  let contenido = document.querySelector("#output-texto");
+  contenido.select();
+  document.execCommand("copy");
 }
 
 focus();
