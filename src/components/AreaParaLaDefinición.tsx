ea11bc58a7bc191bf143dbe79054a21bac29dafc
Luis Angel Ponce Alvarez <poncealvarezluisangel4@gmail.com>
2024-08-23 22:24:01 -0600

    add css and description and proyect

--- src/components/AreaParaLaDefinición.tsx
+++ src/components/AreaParaLaDefinición.tsx
@@ -3,19 +3,20 @@ import "../style/AreaParaLaDefinición.css";
 export function AreaParaLaDefinición() {
   return (
     <div id="area-para-la-definicion">
-      <div className="text">
+      <div className="section-title">
         <h1 className="title">Challenges Encriptador</h1>
-        <p>
-          Este es el primer Desafío del programa ONE - Oracle Next Education, en
-          el que se nos pedía que hiciéramos un encriptador de texto, con el que
-          se pudieran intercambiar mensajes secretos con otras personas que
-          conocieran el secreto de la encriptación utilizada.
-        </p>
+
+        <a href="https://github.com/luisangelponcealvarez/challenges-encriptador">
+          <i className="fa-brands fa-github icon"></i>
+        </a>
       </div>
 
-      <a href="https://github.com/luisangelponcealvarez/challenges-encriptador">
-        <i className="fa-brands fa-github icon"></i>
-      </a>
+      <p className="text">
+        Este es el primer Desafío del programa ONE - Oracle Next Education, en
+        el que se nos pedía que hiciéramos un encriptador de texto, con el que
+        se pudieran intercambiar mensajes secretos con otras personas que
+        conocieran el secreto de la encriptación utilizada.
+      </p>
     </div>
   );
 }
