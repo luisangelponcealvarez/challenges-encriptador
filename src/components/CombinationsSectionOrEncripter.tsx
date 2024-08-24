65f028283b8267bbde8abadd2c981da5d28f5886
Luis Angel Ponce Alvarez <poncealvarezluisangel4@gmail.com>
2024-08-24 15:57:09 -0600

    add and styles

--- src/components/CombinationsSectionOrEncripter.tsx
+++ src/components/CombinationsSectionOrEncripter.tsx
@@ -1,8 +1,13 @@
+import style from "../styles/styles.module.css";
+
 export function CombinationsSectionOrEncripter() {
   return (
-    <section id="combinations-section-or-encripter">
-      <h3 className="title">The encryption "keys" used are the following:</h3>
-      <p className="text">
+    <section>
+      <h3 className={style.title}>
+        The encryption "keys" used are the following:
+      </h3>
+
+      <p className={style.text}>
         The letter "e" is converted for "enter" The letter "i" is converted for
         "imes" The letter "a" is converted for "ai" The letter "o" is converted
         for "ober" The letter "u" is converted for "ufat"
