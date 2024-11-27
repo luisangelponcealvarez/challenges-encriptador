3f6dc74cca7707a37dae4c1e2b2058d8dbc5f881
Luis Angel Ponce Alvarez <poncealvarezluisangel4@gmail.com>
2024-11-26 19:34:35 -0600

    modified files and scripts

--- src/components/CombinationsSectionOrEncripter.tsx
+++ src/components/CombinationsSectionOrEncripter.tsx
@@ -2,16 +2,30 @@ import style from "../styles/styles.module.css";
 
 export function CombinationsSectionOrEncripter() {
   return (
-    <section>
-      <h3 className={style.title}>
-        The encryption "keys" used are the following:
-      </h3>
+    <section className={style.section}>
+      <div className={style.container}>
+        <h3 className={style.title}>
+          The encryption "keys" used are the following:
+        </h3>
 
-      <p className={style.text}>
-        The letter "e" is converted for "enter" The letter "i" is converted for
-        "imes" The letter "a" is converted for "ai" The letter "o" is converted
-        for "ober" The letter "u" is converted for "ufat"
-      </p>
+        <p className={style.text}>
+          The letter "e" is converted for "enter" The letter "i" is converted
+          for "imes" The letter "a" is converted for "ai" The letter "o" is
+          converted for "ober" The letter "u" is converted for "ufat"
+        </p>
+      </div>
+
+      <div className={style.container}>
+        <h3 className={style.title}>
+          The encryption "keys" used are the following:
+        </h3>
+
+        <p className={style.text}>
+          The letter "e" is converted for "enter" The letter "i" is converted
+          for "imes" The letter "a" is converted for "ai" The letter "o" is
+          converted for "ober" The letter "u" is converted for "ufat"
+        </p>
+      </div>
     </section>
   );
 }
